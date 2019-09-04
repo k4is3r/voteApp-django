6c136b57d762376693494978be723f65167f699d
Eduardo Imery <ejwimery@gmail.com>
2019-09-04 13:52:03 -0600

    creating field in admin page

--- votes/polls/admin.py
+++ votes/polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Question,Choice
+
+class ChoiceInLine(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
+        ]
+    inlines = [ChoiceInLine]
+
+#admin.site.register(Question)
+#admin.site.register(Choice)
